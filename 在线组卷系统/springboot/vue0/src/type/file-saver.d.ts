e678a82553591f8af4cb0c3bffa15925d5e65835
syj2347 <86706794+syj2347@users.noreply.github.com>
2023-07-06 11:00:52 +0800

    Add files via upload

---
+++ 在线组卷系统/springboot/vue0/src/type/file-saver.d.ts
@@ -0,0 +1,2 @@
+declare module 'file-saver';
+declare module 'html-docx-js/distml-docx';
---
+++ 在线组卷系统/springboot/vue0/src/views/front/Home.vue
@@ -0,0 +1,87 @@
+<template>
+  <div style="margin-bottom: 100px;">
+
+    <div style="margin: 20px 0; font-size: 20px; color: #1E90FF">列表</div>
+    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 10px; margin: 10px 0" v-for="item in tableData"
+      :key="item.id">
+      <div style="color: #333; margin: 10px 0; font-size: 20px">{{ item.name }}</div>
+      <div style="color: #666; margin: 10px 0;">
+        <span>教室：{{ item.room }}</span>
+        <span style="margin-left: 20px">时间：{{ item.time }}</span>
+        <span style="margin-left: 20px">
+          考试状态：
+          <el-tag v-if="item.state === '已结束'" type="danger">{{ item.state }}</el-tag>
+          <el-tag v-if="item.state === '进行中'" type="success">{{ item.state }}</el-tag>
+          <el-tag v-else>{{ item.state }}</el-tag>
+        </span>
+
+        <div style="margin: 10px 0">
+          <el-button type="primary" @click="sign(item.id)">报名</el-button>
+          <el-button style="margin-left: 20px" type="primary" @click="doPaper(item.id)"
+            v-if="item.enable && item.state === '进行中'">开始考试</el-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontHome",
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 10,
+      name: "",
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {},
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods: {
+    doPaper(examId) {
+      this.$router.push('/front/paper?examId=' + examId)
+    },
+    sign(examId) {
+      let form = { userId: this.user.id, examId: examId }
+      this.request.post("/sign", form).then(res => {
+        if (res.code === '200') {
+          this.$message.success("报名成功")
+        } else {
+          this.$message.error(res.msg)
+        }
+      })
+    },
+    load() {
+      this.request.get("/exam/page/front", {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          name: this.name,
+        }
+      }).then(res => {
+        this.tableData = res.data.records
+        this.total = res.data.total
+      })
+      this.request.get("/course").then(res => {
+        this.courses = res.data
+      })
+    },
+    handleSizeChange(pageSize) {
+      console.log(pageSize)
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum) {
+      console.log(pageNum)
+      this.pageNum = pageNum
+      this.load()
+    },
+  }
+}
+</script>
+
+<style></style>
