73a34c1823c4a327620a8bbe40a93a2e468281d2
syj2347 <86706794+syj2347@users.noreply.github.com>
2023-07-06 10:41:19 +0800

    Add files via upload

---
+++ vue0/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="color: #666;font-size: 14px;">
+    <div style="padding-bottom: 20px">
+      <b>您好！{{ user.nickname }}</b>
+    </div>
+    <el-card>
+      欢迎使用本系统
+      <el-divider />
+    </el-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
+  }
+}
+</script>
